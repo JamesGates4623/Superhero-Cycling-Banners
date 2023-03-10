19932fa520ba906da50fcc248e142f3c670c4e57
JamesGates4623 <jamesgates638@gmail.com>
2023-03-09 21:50:21 -0500

    First Commit

---
+++ slides.js
@@ -0,0 +1,38 @@
+const slides = [
+
+{
+    src: 'superhero banners/images/superhero1.png',
+    url: 'https://en.wikipedia.org/wiki/Superhero'
+
+},
+
+{
+    src: 'superhero banners/images/superhero2.png',
+    url: 'https://www.britannica.com/topic/list-of-superheroes-2024795'
+
+},
+{
+    src: 'superhero banners/images/superhero3.png',
+    url: 'https://www.britannica.com/art/superhero'
+
+},
+{
+    src: 'superhero banners/images/superhero4.png',
+    url: 'https://www.imdb.com/title/tt0078346/?ref_=fn_al_tt_1'
+
+},
+{
+    src: 'superhero banners/images/superhero5.png',
+    url: 'https://editorial.rottentomatoes.com/guide/best-superhero-movies-of-all-time/'
+
+},
+{
+    src: 'superhero banners/images/superhero6.png',
+    url: 'https://www.marvel.com/'
+
+},
+
+
+
+
+];
